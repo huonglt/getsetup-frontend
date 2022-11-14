8ed080eaf9bb0a2a69a8782394ebbea06297efec
Helen Le <lthuongvn@yahoo.com>
2022-11-15 09:08:41 +1300

    add app component

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const App = () => {
-  return <h1>React + Typescript + Webpack setup!</h1>
-}
+import { App } from './app'
 
 ReactDOM.render(
   <React.StrictMode>
