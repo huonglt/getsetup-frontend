d846fe4bc0a454f7c40d934ff5394c4e1673732f
Helen Le <lthuongvn@yahoo.com>
2022-11-15 09:06:53 +1300

    create the app component showing the guide dashboard

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react'
+import { GuideDashboard } from './components/GuideDashboard'
+import './css/app.css'
+
+export const App = () => {
+  return (
+    <div className="App">
+      <GuideDashboard />
+    </div>
+  )
+}
