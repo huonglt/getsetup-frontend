e1837193f574be80c5a17de2a4c2e8e58dfebb50
Helen Le <lthuongvn@yahoo.com>
2022-11-15 17:56:19 +1300

    move the methods to module availability

--- src/apis/availability.ts
+++ src/apis/availability.ts
@@ -2,7 +2,8 @@
  * Method to retrieve teaching availabiltiy for a guide in a given week
  */
 
-import { GET_GUIDE_AVAILABILITY } from './endpoints'
+import { GuideAvailability } from '../types/guide'
+import { GET_GUIDE_AVAILABILITY, POST_GUIDE_AVAILABILITY } from './endpoints'
 
 export const getGuideAvailability = async (
   userId: number,
@@ -24,6 +25,34 @@ export const getGuideAvailability = async (
     return data
   } catch (error) {
     // 403 error, network error etc
+    if (error instanceof Error) {
+      err = error
+    } else {
+      err = new Error(`Some error occured: ${JSON.stringify(error)}`)
+    }
+  }
+  return err
+}
+
+/**
+/**
+ * Method to submit teaching availability for a guide in a given week
+ */
+export const submitAvailability = async (
+  guideAvailability: GuideAvailability
+) => {
+  let err: Error | null = null
+
+  try {
+    const response = await fetch(POST_GUIDE_AVAILABILITY, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(guideAvailability)
+    })
+    return response.json()
+  } catch (error) {
     if (error instanceof Error) {
       err = error
     } else {
--- src/apis/endpoints.ts
+++ src/apis/endpoints.ts
@@ -8,3 +8,9 @@ export const GET_GUIDE_AVAILABILITY = `${HOST}/availability`
 
 // api to submit guide teaching availability
 export const POST_GUIDE_AVAILABILITY = `${HOST}/availability/update`
+
+// api to get all week numbers
+export const GET_WEEK_NUMBERS = `${HOST}/week/weekNumbers`
+
+// api to get week days of a given week
+export const GET_WEEK_DAYS = `${HOST}/week/weekDays`
