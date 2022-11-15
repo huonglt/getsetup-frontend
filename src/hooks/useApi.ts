95a341482f4a7e65c1070b069ff7e7da489ca87e
Helen Le <lthuongvn@yahoo.com>
2022-11-16 11:13:37 +1300

    only update state when component is mounted

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -1,35 +1,43 @@
 import { useCallback, useState } from 'react'
+import { useIsMounted } from './useIsMounted'
 
 /**
  * Hook to call apiService
  * Manage state of api call: is loading, is error, or loaded with data
  * Also allow user to retry to call a api service if any error occurred
  */
-
 export const useApi = <T>(apiService: (...args: any[]) => Promise<any>) => {
   const [isLoading, setIsLoading] = useState(false)
   const [isError, setIsError] = useState(false)
   const [data, setData] = useState<T | undefined>(undefined)
+  const isMounted = useIsMounted()
 
+  console.log(`isMounted = ${isMounted.current}`)
   /**
    * load data. wrap in useCallback to avoid function re-created after each render
    */
   const loadData = useCallback(
     async (...args: any[]) => {
+      if (!isMounted.current) {
+        return
+      }
+
       setIsError(false)
       setIsLoading(true)
       setData(undefined)
       try {
         const result = await apiService(...args)
-        if (result instanceof Error) {
-          setIsError(true)
-        } else {
-          setData(result as T)
+        if (isMounted.current) {
+          if (result instanceof Error) {
+            setIsError(true)
+          } else {
+            setData(result as T)
+          }
         }
       } catch (err) {
-        setIsError(true)
+        if (isMounted.current) setIsError(true)
       } finally {
-        setIsLoading(false)
+        if (isMounted.current) setIsLoading(false)
       }
     },
     [apiService]
