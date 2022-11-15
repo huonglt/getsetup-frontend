912e72d0cc6e573ea6963a3518aa6afa8250c74a
Helen Le <lthuongvn@yahoo.com>
2022-11-15 17:58:46 +1300

    add endpoint to get list of guides

--- src/apis/endpoints.ts
+++ src/apis/endpoints.ts
@@ -3,14 +3,17 @@
  */
 const HOST = 'http://localhost:3000'
 
-// api to retrieve guide teaching availability
+// endpoint to retrieve guide teaching availability
 export const GET_GUIDE_AVAILABILITY = `${HOST}/availability`
 
-// api to submit guide teaching availability
+// endpoint to submit guide teaching availability
 export const POST_GUIDE_AVAILABILITY = `${HOST}/availability/update`
 
-// api to get all week numbers
+// endpoint to get all week numbers
 export const GET_WEEK_NUMBERS = `${HOST}/week/weekNumbers`
 
-// api to get week days of a given week
+// endpoint to get week days of a given week
 export const GET_WEEK_DAYS = `${HOST}/week/weekDays`
+
+// endpoint to get list of guides
+export const GET_GUIDE_LIST = `${HOST}/guide/list`
